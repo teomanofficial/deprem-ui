10db3f45970367999c74d83dfeb6762f3e026034
Malcolms34 <59875950+Malcolms34@users.noreply.github.com>
2023-02-09 23:00:26 +0300

    Contact Page design

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ContactComponent {
+
+}
+
+
